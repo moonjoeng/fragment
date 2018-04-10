fe2e4993cfed7b151728ff87ee36c54ad95a451a
songv <smj1979@ajou.ac.kr>
2018-04-10 18:24:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/songv/fragment/FragmentB.java
@@ -0,0 +1,18 @@
+package com.example.songv.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by songv on 2018-04-10.
+ */
+
+public class FragmentB extends Fragment {
+    @Override //인플레이트
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            return inflater.inflate(R.layout.fragment_b, container, false);
+        }
+}
---
+++ app/src/main/java/com/example/songv/fragment/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.songv.fragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        linearLayout=findViewById(R.id.linear);*/
+
+        if (linearLayout!=null){
+            if (savedInstanceState!=null){
+                return;
+            }
+        }
+
+        FragmentA firstFragment = new FragmentA();//프레그먼트 생성
+        firstFragment.setArguments(getIntent().getExtras());
+        //프레그먼트 매니저로 트랜잭션 시작, 컨테이너에 first 추가함
+        getFragmentManager().beginTransaction().add(R.id.linear, firstFragment).commit();
+    }
+
+    public void selectFragment(View view){
+        Fragment fr = null;
+
+        switch (view.getId()){
+            case R.id.button1:
+                fr=new FragmentA();
+                break;
+            case R.id.button2:
+                fr=new FragmentB();
+                break;
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.linear, fr);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+   /* public void onClick1(View v){
+
+    }*/
+}
